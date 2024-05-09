6ee356ec872dc4f5779df51575898f5bb74e9d18
aniketraut16 <rautan_1@rknec.edu>
2024-05-09 21:24:39 +0530

    Added User And Admin Auth Middlewares

---
+++ backend/Middlewares/adminauthentication.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const { Admin } = require("../Models/Admin");
+
+const adminAuth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const admin = await Admin.findOne({ username: decoded.username });
+
+    if (!admin) {
+      return res.status(401).json({ error: "Admin not found" });
+    }
+
+    req.username = decoded.username;
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+};
+
+module.exports = { adminAuth };
