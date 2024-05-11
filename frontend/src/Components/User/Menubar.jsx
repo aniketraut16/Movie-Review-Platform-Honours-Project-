574d674c528cc823811df20e56882d217cca09c0
aniketraut16 <rautan_1@rknec.edu>
2024-05-11 20:48:07 +0530

    Completed Menu bar for navigation

--- frontend/src/Components/User/Menubar.jsx
+++ frontend/src/Components/User/Menubar.jsx
@@ -1,10 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 function Menubar() {
+  const loc = useLocation();
   return (
     <>
       <Outlet />
-      <div id="menubar"></div>;
+      <div id="menubar">
+        <Link to={``} className={loc.pathname === "/" ? "active" : ""}>
+          <i className="fa-solid fa-house"></i>
+        </Link>
+        <Link
+          to={`postreview`}
+          className={loc.pathname === "/postreview" ? "active" : ""}
+        >
+          <i className="fa-solid fa-circle-plus"></i>
+        </Link>
+        <Link
+          to={`profile`}
+          className={loc.pathname === "/profile" ? "active" : ""}
+        >
+          <i className="fa-solid fa-user"></i>
+        </Link>
+      </div>
+      ;
     </>
   );
 }
